03c1180b17b65d9ba4f44ce75adb63de797eda19
Al Siam <alsiam.dev@gmail.com>
2023-02-16 20:10:38 +0600

    web lab: project category added and updated

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-window.onload = function () {
-    const projectList = projects.map(project => {
-        return `
+window.onload = () => {
+  const featuredProjectList = featuredProjects.map((project) => {
+    return `
+            <div class="bg-slate-100 p-3 rounded shadow-lg">
+                <div class="">
+                    <img src=${project.thumbnail} alt="hamburger food" class="w-full h-full">
+                </div>
+                <div class="flex justify-between items-center mt-3">
+                    <h1 class="text-slate-800 text-lg">${project.name}</h1>
+                    <div class="flex justify-between items-center gap-3">
+                        <a class="bg-slate-200 text-slate-800 py-1 px-3 hover:bg-slate-300 hover:text-slate-900 rounded"
+                            href=${project.code}
+                            target="_blank">Source Code</a>
+                        <a class="bg-slate-200 text-slate-800 py-1 px-3 hover:bg-slate-300 hover:text-slate-900 rounded"
+                            href=${project.preview} target="_blank">Preview
+                            Demo</a>
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+  document.getElementById("featured-projects").innerHTML =
+    featuredProjectList.join("");
+  const totalProjectList = totalProjects.map((project) => {
+    return `
         <div class="bg-slate-100 p-3 rounded shadow-lg">
             <div class="">
                 <img src=${project.thumbnail} alt="hamburger food" class="w-full h-full">
@@ -17,9 +39,9 @@ window.onload = function () {
                 </div>
             </div>
         </div>
-    `
-    })
+    `;
+  });
 
-    document.getElementById("project").innerHTML = projectList.join("")
-
-}
+  document.getElementById("total-projects").innerHTML =
+    totalProjectList.join("");
+};
