c5e93f7e1421a3ddd3254ecf7dea4368caa885b5
Al Siam <alsiam.dev@gmail.com>
2023-02-21 13:51:24 +0600

    web projects: converted to responsive

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ window.onload = () => {
                     <img src=${project.thumbnail} alt="hamburger food" class="w-full h-full">
                 </div>
                 <div class="flex justify-between items-center mt-3">
-                    <h1 class="text-slate-800 text-lg">${project.name}</h1>
+                    <h1 class="text-slate-800 text-sm md:text-lg font-semibold">${project.name}</h1>
                     <div class="flex justify-between items-center gap-3">
-                        <a class="bg-slate-200 text-slate-800 py-1 px-3 hover:bg-slate-300 hover:text-slate-900 rounded"
+                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
                             href=${project.code}
                             target="_blank">Source Code</a>
-                        <a class="bg-slate-200 text-slate-800 py-1 px-3 hover:bg-slate-300 hover:text-slate-900 rounded"
+                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
                             href=${project.preview} target="_blank">Preview
                             Demo</a>
                     </div>
@@ -24,29 +24,29 @@ window.onload = () => {
     featuredProjectList.join("");
   const totalProjectList = totalProjects.map((project) => {
     return `
-        <div class="bg-slate-100 p-3 rounded shadow-lg">
-            <div class="">
-                <img src=${project.thumbnail} alt="hamburger food" class="w-full h-full">
-            </div>
-            <div class="flex justify-between items-center mt-3">
-                <h1 class="text-slate-800 text-lg">${project.name}</h1>
-                <div class="flex justify-between items-center gap-3">
-                    <a class="bg-slate-200 text-slate-800 py-1 px-3 hover:bg-slate-300 hover:text-slate-900 rounded"
-                        href=${project.code}
-                        target="_blank">Source Code</a>
-                    <a class="bg-slate-200 text-slate-800 py-1 px-3 hover:bg-slate-300 hover:text-slate-900 rounded"
-                        href=${project.preview} target="_blank">Preview
-                        Demo</a>
+            <div class="bg-slate-100 p-3 rounded shadow-lg">
+                <div class="">
+                    <img src=${project.thumbnail} alt="hamburger food" class="w-full h-full">
+                </div>
+                <div class="flex justify-between items-center mt-3">
+                    <h1 class="text-slate-800 text-sm md:text-lg font-semibold">${project.name}</h1>
+                    <div class="flex justify-between items-center gap-3">
+                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
+                            href=${project.code}
+                            target="_blank">Source Code</a>
+                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
+                            href=${project.preview} target="_blank">Preview
+                            Demo</a>
+                    </div>
                 </div>
             </div>
-        </div>
     `;
   });
 
   document.getElementById("total-projects").innerHTML =
     totalProjectList.join("");
-    
+
   setTimeout(() => {
-      document.getElementById("preloader").style.display = "none";
-    }, 2000);
+    document.getElementById("preloader").style.display = "none";
+  }, 2000);
 };
