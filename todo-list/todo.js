23ee228086e2b1ceae784aa1ed35d39e76062642
Al Siam <alsiam.dev@gmail.com>
2023-05-17 12:57:31 +0600

    html projects: updated

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,7 +1,7 @@
 const localStorageList = () => {
     const list = getList();
-    for (const taskName in list) {
-        displayList(taskName)
+    for (const name in list) {
+        displayList(name)
     }
 }
 const addItem = () => {
@@ -21,10 +21,10 @@ const addItem = () => {
     inputField.value = '';
 
 }
-const displayList = taskName => {
+const displayList = name => {
     const ul = document.getElementById('ul-list');
     const li = document.createElement('li');
-    li.innerHTML = taskName;
+    li.innerHTML = name;
 
     ul.appendChild(li);
 }
@@ -39,12 +39,12 @@ const getList = () => {
     }
     return listObject;
 }
-const addList = taskName => {
+const addList = name => {
     const list = getList();
-    if (list[taskName]) {
-        list[taskName] = list[taskName] + 1;
+    if (list[name]) {
+        list[name] = list[name] + 1;
     } else {
-        list[taskName] = 1;
+        list[name] = 1;
     }
     const listStringified = JSON.stringify(list);
     localStorage.setItem('list', listStringified);
